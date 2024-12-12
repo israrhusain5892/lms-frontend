48ceb08e4a4ece82bfec03a4927a8dd91a9a1761
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-12 14:20:14 +0530

    feat: add state and handler for mobile no. input
    - Added a state variable `mobileNumber` to store and manage the user's mobile number.
    - Initialized the state with an empty string as the default value.
    - Provided the `setMobileNumber` function to update the state dynamically.
    - Implemented `handleMobileChange`, an event handler that updates `mobileNumber` based on user input.
    - This setup enables controlled input for the mobile number field in the signup form.

--- src/Pages/SignUpPage.jsx
+++ src/Pages/SignUpPage.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 
 function SignUpPage() {
+    const [userMobileNumber, setUserMobileNumber] = useState(""); // To store an manage user's mobile number
 
+    const handleUserMobileChange = (e) => {
+        setUserMobileNumber(e.target.value);
+    };
 }
 
 export default SignUpPage;
