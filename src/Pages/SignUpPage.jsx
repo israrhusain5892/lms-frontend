09e980cc79b6cd55e9c287ce5db05221f8ab083a
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-14 22:16:17 +0530

    refactor: use rem units and align with Figma specs

--- src/Pages/SignUpPage.jsx
+++ src/Pages/SignUpPage.jsx
@@ -29,7 +29,7 @@ function SignUpPage() {
         e.preventDefault();
 
         // Backend integration here
-        
+
         console.log(`User's mobile number: ${userMobileNumber}`);
     };
 
@@ -37,48 +37,57 @@ function SignUpPage() {
     return (
         <main className="signUpMain">
             <section className="signUpSection">
-                <header className="signUpHeader">
-                    <h1 className="signUpTitle">Create Account</h1>
-                    <p className="signUpDescription">We have sent you a <strong>One Time Password (OTP)</strong> on this mobile number.</p>
-                </header>
+                <div className="signUpContentContainer">
+                    <div className="signUpContent">
+                        <header className="signUpHeader">
+                            <h1 className="signUpTitle">Create Account</h1>
+                            <p className="signUpDescription">We have sent you a <strong>One Time Password (OTP)</strong> on this mobile number.</p>
+                        </header>
 
-                <form action="" className="signUpForm" onSubmit={handleUserMobilePhoneSubmit}>
-                    <fieldset className="signUpFormFieldset">
-                        <div className="signUpFormLabelContainer">
-                            <label htmlFor="userMobileNumber" className="signUpFormLabel">Enter mobile no.*</label>
+                        <div className="signUpFormContainer">
+                        <form action="" className="signUpForm" onSubmit={handleUserMobilePhoneSubmit}>
+                            <fieldset className="signUpFormFieldset">
+                                <div className="signUpFormLabelContainer">
+                                    <label htmlFor="userMobileNumber" className="signUpFormLabel">Enter mobile no.*</label>
+                                </div>
+
+                                <div className="signUpFormInputContainer">
+                                    <select id="countryCode" name="countryCode" defaultValue="+91" className="signUpFormCountryCodeSelect">
+                                        <option value="+91" className="signUpFormCountryCodeOption">+91</option> {/* India */}
+                                        <option value="+92" className="signUpFormCountryCodeOption">+92</option> {/* Pakistan */}
+                                        <option value="+92" className="signUpFormCountryCodeOption">+94</option> {/* Sri Lanka */}
+                                        <option value="+92" className="signUpFormCountryCodeOption">+95</option> {/* Mayanmar */}
+                                        <option value="+92" className="signUpFormCountryCodeOption">+977</option> {/* Nepal */}
+                                        {/* Refactor to generate automatically if need to display all codes. */}
+                                    </select>
+                                    <input
+                                        type="tel"
+                                        id="userMobileNumber"
+                                        className="signUpFormUserMobileNumberInput"
+                                        name="userMobileNumber"
+                                        value={userMobileNumber}
+                                        onChange={handleUserMobileChange}
+                                        required
+                                    />
+                                </div>
+                                {errorMessage && <p className="signUpFormErrorText">{errorMessage}</p>}
+                            </fieldset>
+                        </form>
+
+                        {/* Refactor later to use "Link" */}
+                        <p>
+                            Don’t have account? <a href="#">Continue without account</a>
+                        </p>
+
                         </div>
+                    </div>
 
-                        <div className="signUpFormInputContainer">
-                            <select id="countryCode" name="countryCode" defaultValue="+91" className="signUpFormCountryCodeSelect">
-                                <option value="+91" className="signUpFormCountryCodeOption">+91</option> {/* India */}
-                                <option value="+92" className="signUpFormCountryCodeOption">+92</option> {/* Pakistan */}
-                                <option value="+92" className="signUpFormCountryCodeOption">+94</option> {/* Sri Lanka */}
-                                <option value="+92" className="signUpFormCountryCodeOption">+95</option> {/* Mayanmar */}
-                                <option value="+92" className="signUpFormCountryCodeOption">+977</option> {/* Nepal */}
-                                {/* Refactor to generate automatically if need to display all codes. */}
-                            </select>
-                            <input
-                                type="tel"
-                                id="userMobileNumber"
-                                className="signUpFormUserMobileNumberInput"
-                                name="userMobileNumber"
-                                value={userMobileNumber}
-                                onChange={handleUserMobileChange}
-                                required
-                            />
-                        </div>
-                        {errorMessage && <p className="signUpFormErrorText">{errorMessage}</p>}
-                    </fieldset>
-
-                    {/* Refactor later to use "Link" */}
-                    <p>
-                        Don’t have account? <a href="#">Continue without account</a>
-                    </p>
-
-                    <button type="submit" className="signUpFormGetCodeButton" disabled={!isMobileValid}>
-                        Get Code
-                    </button>
-                </form>
+                    <div className="signUpFormGetCodeButtonContainer">
+                        <button type="submit" className="signUpFormGetCodeButton" disabled={!isMobileValid}>
+                            Get Code
+                        </button>
+                    </div>
+                </div>
 
                 <footer className="signUpFooter">
                     <p className="signUpFooterText">Or Register with</p>
