f9d2fe89851497f6243865a008b333148c314c45
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-14 23:58:54 +0530

    refactor: use rem units and match Figma specs

--- src/Pages/OTPVerification.jsx
+++ src/Pages/OTPVerification.jsx
@@ -1,56 +1,63 @@
 function OTPVerification() {
-    
     return (
         <main className="OTPVerificationMain">
             <section className="OTPVerificationSection">
+                <div className="OTPVerificationContentContainer">
+                    <header className="OTPVerificationHeader">
+                        <h1 className="OTPVerificationTitle">Enter Code</h1>
+                        <p className="OTPVerificationDescription">We’ve sent an SMS with an activation code to your phone <span>+33 2 94 27 84 11</span></p>
+                    </header>
 
-                <header className="OTPVerificationHeader">
-                    <h1 className="OTPVerificationTitle">Enter Code</h1>
-                    <p className="OTPVerificationDescription">We’ve sent an SMS with an activation code to your phone +33 2 94 27 84 11</p>
-                </header>
+                    <form action="" className="OTPVerificationForm">
+                        <fieldset className="OTPVerificationFormFieldset">
+                            <div className="OTPVerificationInputContainer">
+                                {/* Todo: Conditionally handle adding a an error class when invalid otp is entered. */}
+                                <input
+                                    type="number"
+                                    className="userOTPInput"
+                                    maxLength="1"
+                                />
+                                <input
+                                    type="number"
+                                    className="userOTPInput"
+                                    maxLength="1"
+                                />
+                                <input
+                                    type="number"
+                                    className="userOTPInput"
+                                    maxLength="1"
+                                />
+                                <input
+                                    type="number"
+                                    className="userOTPInput"
+                                    maxLength="1"
+                                />
+                                <input
+                                    type="number"
+                                    className="userOTPInput"
+                                    maxLength="1"
+                                />
+                                <input
+                                    type="number"
+                                    className="userOTPInput"
+                                    maxLength="1"
+                                />
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
 
-                <form action="" className="OTPVerificationForm">
-                    <fieldset className="OTPVerificationFormFieldset">
-                        <div className="OTPVerificationInputContainer">
-                            <input
-                                type="number"
-                                className="userOTPInput"
-                                maxLength="1"
-                            />
-                            <input
-                                type="number"
-                                className="userOTPInput"
-                                maxLength="1"
-                            />
-                            <input
-                                type="number"
-                                className="userOTPInput"
-                                maxLength="1"
-                            />
-                            <input
-                                type="number"
-                                className="userOTPInput"
-                                maxLength="1"
-                            />
-                            <input
-                                type="number"
-                                className="userOTPInput"
-                                maxLength="1"
-                            />
-                            <input
-                                type="number"
-                                className="userOTPInput"
-                                maxLength="1"
-                            />
-                        </div>
-                        {/* Error message here */}
-                    </fieldset>
-                </form>
+                {/*
+                <p>
+                    Error text!
+                </p>
+                */}
 
                 <footer className="OTPVerificationFooter">
-                    <p className="OTPVerificationFooterText">Send code again</p>
+                    {/* Todo: Refactor the following into an actionable link that re-sends otp  */}
+                    <p className="OTPVerificationFooterText">Send code again</p> 
+                    {/* Todo: Refactor the following to display a live timer */}
                     <p className="OTPVerificationFooterTimer">00:20</p>
-                    {/* Handle a dynamic timer later */}
                 </footer>
 
             </section>
