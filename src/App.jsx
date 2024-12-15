0a6e73844d4d490587488bf23c48f83ed2f9d814
israrhusain5892 <israrhusain5892@gmail.com>
2024-12-15 11:21:56 +0530

    changes main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import AssignmentPage from './Pages/loginpage'
+
 
 function App() {
   
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
         <div>
-           <AssignmentPage/>
+          
         </div>
     </>
   )
