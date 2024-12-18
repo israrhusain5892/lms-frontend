980e10706b0ed4a38bbc3fa111ba42ea933bd442
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-12-18 14:39:58 +0530

    docs: setup contribution guide

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-// import a component here
+// USE THIS TO VIEW YOUR COMPONENT RENDER
+// Step 1: import your component here
 
 function App() {
   return (
     <>
-      {/* Render the component here */}
+      {/* Step 2: Use it here */}
     </>
   );
 }
