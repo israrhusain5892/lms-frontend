1eb4e98de52e717d20de5c3cd09e47c3eb4ce87f
sarbajit <sarbajit>
2025-01-01 20:43:15 +0530

    Added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 
-import OtpVerification from './components/OtpVerification/OtpVerification';
 import Header from './components/Header/Header';
-
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-
+import Popup from './components/PopUps/PopUps';
+import Loader from './components/Loader/Loader';
 
 function App() {
   
@@ -12,8 +10,27 @@ function App() {
     <>
         
            <Header/>
-           <OtpVerification/>
-        
+
+           <Popup
+      title="Are You Sure You Want To Leave?"
+      description="You haven't completed this assignment yet. Are you sure you want to leave without submitting it?"
+      onContinue={() => alert("You chose to continue!")}
+    />
+           <Popup
+      title="Ready to Submit?"
+      description="Are you sure you want to submit the test? Please double-check your answers before proceeding."
+      onContinue={() => alert("You chose to continue!")}
+    />
+
+<Popup
+  title="Missing or Invalid Answers"
+  description="We detected some incomplete or invalid answers. Do you still want to submit this assessment?"
+  status="wrong"
+  onContinue={() => console.log("Continuing...")}
+/>
+
+                  
+            <Loader/>
     </>
   )
 }
