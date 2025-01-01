5a1f3e7d276316ea4c4b9fb4d74422e57dbc5067
israrhusain5892 <israrhusain5892@gmail.com>
2025-01-01 23:14:48 +0530

    complete quiz page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 // USE THIS TO VIEW YOUR COMPONENT RENDER
 // Step 1: import your component here
 
+import QuizPage from "./pages/QuizPage/QuizPage";
+
 function App() {
   return (
     <>
       {/* Step 2: Use it here */}
+      <QuizPage/>
     </>
   );
 }
