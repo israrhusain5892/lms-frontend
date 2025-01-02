35c0452b3f94b47df47ab2038d1bea89eb25ad75
sarbajit <sarbajit>
2025-01-02 22:19:35 +0530

    Added extra styles

--- src/components/CourseDetails/CourseDetails.jsx
+++ src/components/CourseDetails/CourseDetails.jsx
@@ -1,9 +1,9 @@
-import  { useState } from 'react';
-import courseMaterialsData from '../../utils/courseMaterials/courseMaterials.json';
+import { useState } from "react";
+import courseMaterialsData from "../../utils/courseMaterials/courseMaterials.json";
 import "./coursedetails.css";
-import Details from './Courses';
+import Details from "./Courses";
 import pdf from "../../assets/images/pdf.png";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const CourseDetails = () => {
   const [openMaterial, setOpenMaterial] = useState(null);
@@ -20,7 +20,6 @@ const CourseDetails = () => {
 
       <header className="mb-6">
         <h1 className="text-2xl font-semibold">Data Structure & Algorithms</h1>
-        
       </header>
 
       <section className="mb-4 flex justify-between items-center">
@@ -34,33 +33,39 @@ const CourseDetails = () => {
       </section>
 
       <section>
-        <h2 className="text-2xl font-extrabold mb-4 text-[#3D3D3D]">Course Materials</h2>
+        <h2 className="text-2xl font-extrabold mb-4 text-[#3D3D3D]">
+          Course Materials
+        </h2>
         <ul className="space-y-2">
           {courseMaterialsData.map((material) => (
             <li key={material.id}>
-  <div
-    className="flex justify-between items-center px-4 py-2 bg-gray-100 rounded cursor-pointer hover:bg-gray-200"
-    onClick={() => handleToggle(material.id)}
-  >
-    <span className="flex items-center">
-      <i className={`${material.icon} mr-2`}></i>
-      {material.title}
-    </span>
-    <span>{openMaterial === material.id ? <i className="fa-solid fa-chevron-up"></i> : <i className="fa-solid fa-chevron-down"></i>}</span>
-  </div>
-  {openMaterial === material.id && (
-    material.title === "HTML and CSS Basics" ? (
-      <Details />
-    ) : (
-      <div className="p-4 bg-gray-50 border border-gray-200">
-        <p className="text-gray-600 text-sm">
-          Content for {material.title} will be displayed here.
-        </p>
-      </div>
-    )
-  )}
-</li>
-
+              <div
+                className="flex justify-between items-center px-4 py-2 bg-gray-50 border-b border-[#5555552f] rounded cursor-pointer hover:bg-gray-200"
+                onClick={() => handleToggle(material.id)}
+              >
+                <span className="flex items-center">
+                  <i className={`${material.icon} mr-2`}></i>
+                  {material.title}
+                </span>
+                <span>
+                  {openMaterial === material.id ? (
+                    <i className="fa-solid fa-chevron-up"></i>
+                  ) : (
+                    <i className="fa-solid fa-chevron-down"></i>
+                  )}
+                </span>
+              </div>
+              {openMaterial === material.id &&
+                (material.title === "HTML and CSS Basics" ? (
+                  <Details />
+                ) : (
+                  <div className="p-4 bg-gray-50 border border-gray-200">
+                    <p className="text-gray-600 text-sm">
+                      Content for {material.title} will be displayed here.
+                    </p>
+                  </div>
+                ))}
+            </li>
           ))}
         </ul>
       </section>
