b4f873f0b7cbe68b9c8cbc28d65166ee204ed31b
israrhusain5892 <israrhusain5892@gmail.com>
2025-01-22 18:55:33 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@
 // Step 1: import your component here
 import QuizPage from "./pages/QuizPage/QuizPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 
 function App() {
   return (
     <>
       {/* Step 2: Use it here */}
-      <QuizPage/>
+      <>
+   
+   <Router>
+     <Routes>
+       <Route path="/" element={<AssignmentCarousel />} />
+       <Route path="/next-page" element={<NextPage />} />
+       <Route path="/quiz-page" element={<QuizPage/>} />
+     </Routes>
+   </Router>
+   </>
+      
 
     </>
   );
