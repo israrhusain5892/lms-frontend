fbd2fb2b2dc797d2cfff70cec74e05572c200ca1
preranabiradar <preranab2000@gmail.com>
2025-01-24 16:24:53 +0530

    Created a signin page file similar to the signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import SignUpPage from "./components/SignUpPage/SignUpPage"
+import SignInPage from "./components/SignUpPage/SignInPage"
 
 function App() {
-  
+
 
   return (
     <>
       <SignUpPage />
+      <SignInPage />
+
     </>
   )
 }
