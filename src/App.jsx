17c5e9a34b77d3ba1c6e8a4b11084bce2c3f096e
israrhusain5892 <israrhusain5892@gmail.com>
2025-01-28 13:23:17 +0530

    mm

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 // import OtpVerification from './components/OtpVerification/OtpVerification';
+import './App.css';
 import Header from './components/Header/Header';
 import HeroSection from './components/HeroSection';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
