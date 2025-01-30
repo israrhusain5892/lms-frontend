d37bf2819c5c15d9ef5214d534d648c2a7dd83d2
sarbajit <sarbajit>
2025-01-30 20:07:43 +0530

    Commit Footer part

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import DSACourses from "./components/DSA-Courses/DSA-Courses";
+import Footer from "./components/DSA-Courses/Footers/Footer";
 
 const App = () => {
   return (
+    <>
+
+
     <div className="flex">
 
 <DSACourses/>
 <DSACourses/>
+
       
     </div>
+
+    <Footer/>
+    </>
   )
 }
 
