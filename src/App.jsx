bb892f71ff8553350eeab5b4242c1d28560ad237
sarbajit <sarbajit>
2025-01-30 19:38:27 +0530

    Created Courses Demo file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignUpPage from "./components/SignUpPage/SignUpPage";
-import SuccessPage from "./components/SignUpPage/SuccessPage";
+import DSACourses from "./components/DSA-Courses/DSA-Courses";
 
-function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<SignUpPage />} />
-                <Route path="/home" element={<SuccessPage />} />
-            </Routes>
-        </Router>
-    );
+const App = () => {
+  return (
+    <div className="flex">
+
+<DSACourses/>
+<DSACourses/>
+      
+    </div>
+  )
 }
 
-export default App;
+export default App
