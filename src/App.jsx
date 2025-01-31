3244e771622ac561b68cdbaee948c2ec4bee50fe
sarbajit <sarbajit>
2025-01-31 20:54:41 +0530

    added admin panel management panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // USE THIS TO VIEW YOUR COMPONENT RENDER
 // Step 1: import your component here
+import "./index.css";
+import StudentManagement from "./components/studentData/studentData";
 
 function App() {
   return (
     <>
-      {/* Step 2: Use it here */}
+      <StudentManagement/>
     </>
   );
 }
