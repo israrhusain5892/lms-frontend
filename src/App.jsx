b824010ea86ba79d61e072aca5b9c35c585cf1c8
preranabiradar <preranab2000@gmail.com>
2025-02-03 12:52:33 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
-// USE THIS TO VIEW YOUR COMPONENT RENDER
-// Step 1: import your component here
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AssignmentCarousel from './Pages/OnBoarding/OnBoarding';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import SignUpPage from './Pages/SignupPage/SignUpPage';
+import './App.css';
+import OTPVerification from './Pages/OtpVerifyPage/OTPVerification';
+import OtpVerify from './Pages/OtpVerify/OtpVerify';
+import SuccsessPage from './Pages/SuccessPage';
+// import ProgressBar from './components/ProgressBar/ProgressBar';
+// import CourseDetails from './components/CourseDetails/CourseDetails';
+// import Footer from './components/Footer/Footer';
+// import Header from './components/Header/Header';
+// import TestsSection from './components/TestsSection/TestsSection';
+// import Courses from "./components/CourseDetails/Courses"
+//  import CourseBody from './components/CourseBody/CourseBody';
 
+// import CourseHero from './components/CourseHero/CourseHero';
+import CourseDetailPage from './Pages/CourseDetailPage/CourseDetailPage';
+import HeroSection from './components/HeroSection/HeroSection'
+import HomePage from './Pages/MainPage/HomePage';
+import TrendingCourse from './Components/TopNewCourses/TopCourse';
+import StudentDashboard from './Pages/StudentDashboard/StudentDashboard';
+// import CourseCard from './components/CourseCard/CourseCard';
 function App() {
   return (
-    <>
-      {/* Step 2: Use it here */}
-    </>
-  );
-}
+    <div style={{ width: '100%' }} >
 
+      <Router>
+        <Routes>
+          <Route path="/" element={<AssignmentCarousel />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/otp" element={<OTPVerification />} />
+          <Route path="/otp-verify" element={<OtpVerify />} />
+          <Route path="/successPage" element={<SuccsessPage />} />
+          <Route path="/course-detail" element={<CourseDetailPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/dashboard" element={<StudentDashboard />} />
+
+        </Routes>
+      </Router>
+
+    </div>
+  )
+}
 
 
 export default App;
