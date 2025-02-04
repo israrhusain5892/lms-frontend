e58c69ac5c42434250d54d5c1dc9323a79eadd11
sarbajit <sarbajit>
2025-02-05 01:02:33 +0530

    Added  styles And refactored DSA Responsiveness

--- src/layouts/StudentDashboardLayout.jsx
+++ src/layouts/StudentDashboardLayout.jsx
@@ -2,23 +2,17 @@ import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
 import SideBar from "../Components/SideBar/SideBar";
 
-const StudentDashboardLayout=({children})=>{
-      return(
-          <div>
-                <Header/>
-                <div className="d-flex  ">
+const StudentDashboardLayout = ({ children }) => {
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
+      <div className="d-flex flex-grow-1">
+        <SideBar />
+        <main className="flex-grow-1 p-3">{children}</main>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-                <SideBar/>
-                
-               
-                {
-                    children
-                }
-
-                </div>
-                
-                <Footer/>
-          </div>
-      )
-}
 export default StudentDashboardLayout;
