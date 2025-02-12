96c0ebd1565f10ae0f2332b7c8d3a8491902df3e
sarbajit <sarbajit>
2025-02-12 12:00:30 +0530

    edited fully course manegement

--- src/components/CourseManagement/CourseManagement.jsx
+++ src/components/CourseManagement/CourseManagement.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import "./CourseManagement.css"
+import "./CourseManagement.css";
+import { FaStar } from "react-icons/fa";
+
 // import data from "../../utils/CourseManagement/courses.json";
 const CourseManagement = () => {
   const [courses, setCourses] = useState([]);
@@ -11,44 +13,44 @@ const CourseManagement = () => {
   }, []);
 
   return (
-    <div className="p-6">
+    <div className="p-6 bg-[#F8FAFC]">
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-2xl font-bold  fontstyle underline underline-offset-8 decoration-[#D0D0D0]">Course Management</h1>
         <button className="bg-[#3B76F6] buttonfont text-white text-base font-bold px-4 py-3 rounded-md hover:bg-blue-600">
           Add Course
         </button>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {courses.map((course) => (
           <div
             key={course.id}
-            className="bg-white rounded-md border-[#d9e2ff]  border-2 p-4 hover:shadow-md transition"
+            className="bg-white cards rounded-md border-[#d9e2ff] font-semibold border-2  p-4 hover:shadow-md transition"
           >
             <img
               src={course.image}
               alt={course.title}
-              className="w-full h-40 object-cover rounded-md"
+              className="w-full h-44 object-cover rounded-md"
             />
             <h3 className="mt-4 font-semibold">{course.title}</h3>
-            <p className="text-gray-500 text-sm">{course.instructor} • {course.duration}</p>
+            <p className="text-[#334155] text-xs">{course.instructor} • {course.duration}</p>
             <div className="flex items-center justify-between mt-2">
-              <p className="text-lg font-bold text-gray-800">
-                {course.price} <span className="line-through text-gray-500">{course.originalPrice}</span>
+              <p className="text-lg text-black flex gap-1 ">
+                {course.price} <span className=" text-base text-[#1719b6]">{course.originalPrice}</span>
               </p>
-              <p className="flex items-center text-sm text-gray-600">
-                ⭐ {course.rating} ({course.reviews})
+              <p className="flex items-center text-sm text-black">
+              <FaStar className="text-[#FB923C]"/> {course.rating} ({course.reviews})
               </p>
             </div>
             {course.badge && (
               <span
-                className={`mt-2 inline-block px-3 py-1 text-xs font-semibold rounded-md ${
+                className={`mt-2 inline-block px-3 py-1 text-xs font-medium rounded-md ${
                   course.badge === "Bestseller"
-                    ? "bg-red-100 text-red-600"
+                    ? "bg-[#FFC7CB] text-black"
                     : course.badge === "Top Author"
-                    ? "bg-purple-100 text-purple-600"
+                    ? "bg-[#E7D5FF] text-black"
                     : course.badge === "Editor's Choice"
-                    ? "bg-green-100 text-green-600"
-                    : "bg-blue-100 text-blue-600"
+                    ? "bg-[#DCFCE7] text-black"
+                    : "bg-blue-100 text-black"
                 }`}
               >
                 {course.badge}
