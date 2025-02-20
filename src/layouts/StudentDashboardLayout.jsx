c58912b32a884b282549cdead86abf88e7dc7257
israrhusain5892 <israrhusain5892@gmail.com>
2025-02-20 17:10:30 +0530

    mnn

--- src/layouts/StudentDashboardLayout.jsx
+++ src/layouts/StudentDashboardLayout.jsx
@@ -1,6 +1,6 @@
-import Header from "../Components/Header/Header";
-import Footer from "../Components/Footer/Footer";
-import SideBar from "../Components/SideBar/SideBar";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import SideBar from "../components/SideBar/SideBar";
 
 const StudentDashboardLayout = ({ children }) => {
   return (
